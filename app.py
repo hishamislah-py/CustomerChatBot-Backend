112b13ff673bde127aa712946d82bc822d4ecec7
shaibinkb16082002 <shaibin@arttechgroup.com>
2025-11-26 14:00:19 +0530

    uvicorn

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import glob
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
